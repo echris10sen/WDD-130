4e9a1b270cef7a8e9763dba6e0c47c0c5eef0a04
echris10sen <chr15016@byui.edu>
2022-09-21 08:43:52 -0600

    Ready for github pages!

---
+++ week2/wednesdayJS.js
@@ -0,0 +1,10 @@
+function doSomething() {
+    let text1 = "I did something awesome";
+    let text2 = "...";
+    let text3 = "Owww";
+    let text4 = "Could you stop clicking";
+    let text5 = "STOP!!!";
+    let text6 = "Fine Ill go back";
+    document.getElementById('para1').innerHTML = "I did something awesome";
+
+}
