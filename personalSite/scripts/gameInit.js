074bc78fcf174893757cc0ee8b151b77cead4cbf
echris10sen <chr15016@byui.edu>
2022-11-21 09:03:12 -0700

    updating things

--- personalSite/scripts/gameInit.js
+++ personalSite/scripts/gameInit.js
@@ -3,6 +3,7 @@ function addListeners() {
     console.log("heyo");
     let game = new gameRunner();
     game.gameRunner();
+    
     document.getElementsByClassName("startButton")[0].addEventListener('click', () => game.runGame());
 };
 addListeners();
